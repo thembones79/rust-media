fbe85cfabdae146f9e38c2e48bff296a242a4256
thembones79 <michal.szumnarski@gmail.com>
2024-12-02 10:45:23 +0100

    enum

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
+#[derive(Debug)]
 enum Media {
     Book { title: String, author: String },
     Movie { title: String, director: String },
     Audiobook { title: String },
 }
 
+fn print_media(media: Media) {
+    println!("{:#?}", media);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let audiobook = Media::Audiobook {
+        title: String::from("An Audiobook"),
+    };
+    print_media(audiobook);
 }
