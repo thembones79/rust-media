aac4c6df55172237ab318171a1acd56abda0f473
thembones79 <michal.szumnarski@gmail.com>
2024-12-15 15:56:53 +0100

    import and export modules

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,6 @@
-#[derive(Debug)]
-enum Media {
-    Book { title: String, author: String },
-    Movie { title: String, director: String },
-    Audiobook { title: String },
-    Podcast(u32),
-    Placeholder,
-}
-
-impl Media {
-    fn description(&self) -> String {
-        //     if let Media::Book { title, author } = self {
-        //         format!("Book: {} {}", title, author)
-        //     } else if let Media::Movie { title, director } = self {
-        //         format!("Movie: {} {}", title, director)
-        //     } else if let Media::Audiobook { title } = self {
-        //         format!("Audiobook: {}", title)
-        //     } else {
-        //         String::from("Media Description")
-        //     }
-
-        match self {
-            Media::Book { title, author } => {
-                format!("Book: {} {}", title, author)
-            }
-            Media::Movie { title, director } => {
-                format!("Movie: {} {}", title, director)
-            }
-            Media::Audiobook { title } => {
-                format!("Audiobook: {} ", title)
-            }
-            Media::Podcast(id) => {
-                format!("Podcast: {}", id)
-            }
-            Media::Placeholder => {
-                format!("Placeholder")
-            }
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Catalog {
-    items: Vec<Media>,
-}
-
-impl Catalog {
-    fn new() -> Self {
-        Catalog { items: vec![] }
-    }
-
-    fn add(&mut self, media: Media) {
-        self.items.push(media);
-    }
-
-    fn get_by_index(&self, index: usize) -> Option<&Media> {
-        if self.items.len() > index {
-            Some(&self.items[index])
-        } else {
-            None
-        }
-    }
-}
+mod content;
+use content::catalog::Catalog;
+use content::media::Media;
 
 #[derive(Debug)]
 enum MightHaveAValue<'a> {
